0baabcef6e1120849f63277dbbd9453b443920db
ajin <ajinpdileepkumar@gmail.com>
2025-09-25 12:39:31 +0530

    inicial commit

--- Frontend/lib/config.dart
+++ Frontend/lib/config.dart
@@ -4,11 +4,7 @@ class AppConfig {
 
   static const String baseUrl = useEmulator
       ? "http://127.0.0.1:5000" // emulator (localhost)  // Emulator
-<<<<<<< HEAD
       : "http://172.25.1.31:5000"; // Real Device (Wi-Fi)
-=======
-      : "http://192.168.43.104:5000"; // Real Device (Wi-Fi)
->>>>>>> 3cfc3c30554243525c9210056e8258a2a2358038
 
   static const String realtime = "$baseUrl/api/realtime";
   static const String forecast = "$baseUrl/api/forecast";
